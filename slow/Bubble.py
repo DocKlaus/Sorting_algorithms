3064465fca4c46f513690cf290a970ec00c17b7f
DocKlaus <doct.klaus@gmail.com>
2025-06-09 14:08:20 +0300

    add bubble and selection

---
+++ slow/Bubble.py
@@ -0,0 +1,13 @@
+a = [1, 7, -3, 9, 0, -67, 34, 12, 45, 1000, 6,  8, -2, 99]
+n = len(a)
+
+for i in range(n - 1):
+    swapped = False
+    for j in range(n - 1 - i):
+        if a[j] > a[j + 1]:
+            a[j], a[j + 1] = a[j + 1], a[j]
+            swapped = True
+    if not swapped:
+        break
+
+print('Отсортированный список:', a)
